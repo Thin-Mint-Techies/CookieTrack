2079dbabf35c9acc26a9f4bd9b6e9635e4ce0df7
Brandon-Faulkner <bxf8125@mavs.uta.edu>
2024-10-10 17:50:22 -0500

    Work done to dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,27 +2,31 @@
 module.exports = {
   content: ['./public/ui/**/*.html'],
   theme: {
-    colors: {
-      'green': '#1a9988',
-      'green-light': '#48ada0',
-      'green-dark': '#157a6d',
-      'orange': '#eb5600',
-      'white': '#ffffff',
-      'off-white': '#d9d9d9',
-      'black': '#343434',
-      'gray' : '#D1D5DB'
-    }, 
-    fontFamily: {
-      sans: ['Lilita One', 'sans-serif']
-    },
-    borderRadius: {
-      'default': '8px'
-    },
-    boxShadow: {
-      'default': '0 3px 6px #00000029, 0 3px 6px #0000003b'
-    },
-    backgroundImage: {
-      'login-img': "url('resources/login-bg.png')"
+    extend: {
+      colors: {
+        'green': '#1a9988',
+        'green-light': '#48ada0',
+        'green-dark': '#157a6d',
+        'orange': '#eb5600',
+        'orange-light': '#ef7833',
+        'orange-dark': '#bc4500',
+        'white': '#ffffff',
+        'off-white': '#d9d9d9',
+        'black': '#343434',
+        'gray': '#D1D5DB'
+      },
+      fontFamily: {
+        sans: ['Lilita One', 'sans-serif']
+      },
+      borderRadius: {
+        'default': '8px'
+      },
+      boxShadow: {
+        'default': '0 3px 6px #00000029, 0 3px 6px #0000003b'
+      },
+      backgroundImage: {
+        'login-img': "url('resources/login-bg.png')"
+      }
     }
   },
   plugins: [],
