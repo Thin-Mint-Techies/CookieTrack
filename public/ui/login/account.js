a2eb5dee312ccb97befa0fab75dd444e5bda08cf
Brandon-Faulkner <bxf8125@mavs.uta.edu>
2024-10-31 12:20:04 -0500

    Got create account working

--- public/ui/login/account.js
+++ public/ui/login/account.js
@@ -18,14 +18,15 @@ const auth = getAuth(app);
 const database = getDatabase(app);
 
 onAuthStateChanged(auth, (user) => {
-    //If logged in head to dashboard
-    if (user) {
-        window.location.href = "../dashboard/dashboard.html"
+    //If logged in and on login page,head to dashboard
+    if (user && window.location.href == "../login/sign-in.html") {
+        //window.location.href = "../dashboard/dashboard.html"
     }
 });
 
 //Sign up variables
-const createName = document.getElementById("create-name");
+const createFName = document.getElementById("create-fname");
+const createLName = document.getElementById("create-lname");
 const createEmail = document.getElementById("create-email");
 const createPassword = document.getElementById("create-password");
 const confirmPassword = document.getElementById("confirm-password");
@@ -37,42 +38,64 @@ createAccount.addEventListener('click', () => {
 });
 
 function createUserAccount() {
-    const nameRegex = new RegExp(/^[a-zA-z]+ [a-zA-z]+$/gm, "gm");
+    const nameRegex = new RegExp(/^[a-zA-Z]+[a-zA-Z-]*$/);
     const emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/gm, "gm");
     const passRegex = new RegExp(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/gm, "gm");
 
-    const nameResult = nameRegex.test(createName.value);
-    const emailResult = emailRegex.test(createEmail.value);
-    const passResult = passRegex.test(createPassword.value);
-    const confPass = (createPassword.value === confirmPassword.value);
-    
+    const fNameResult = nameRegex.test(createFName.value.trim());
+    const lNameResult = nameRegex.test(createLName.value.trim());
+    const emailResult = emailRegex.test(createEmail.value.trim());
+    const passResult = passRegex.test(createPassword.value.trim());
+    const confPass = (createPassword.value.trim() === confirmPassword.value.trim());
+    const termsResult = acceptTerms.checked;
+
     //Check if results are good
-    if (!nameResult) {
-        showToast("Invalid Name", "Please make sure you have entered your first and last name.", STATUS_COLOR.RED, true, 5);
+    if (!fNameResult) {
+        showToast("Missing Information", "Please make sure you have correctly entered your first name.", STATUS_COLOR.RED, true, 5);
+        return;
+    }
+
+    if (!lNameResult) {
+        showToast("Missing Information", "Please make sure you have correctly entered your last name.", STATUS_COLOR.RED, true, 5);
         return;
     }
 
     if (!emailResult) {
-        showToast("Invalid Email", "Please make sure you have entered your email and it is correct.");
+        showToast("Invalid Email", "Please make sure you have entered a valid email.", STATUS_COLOR.RED, true, 5);
+        return;
     }
 
     if (!passResult) {
-        showToast("Invalid Passowrd", "Please make sure you have entered a valid password.");
+        showToast("Invalid Password", "Password must be at least 6 characters and contain 1 special character, 1 uppercase, and 1 number.", STATUS_COLOR.RED, true, 8);
+        return;
     }
 
     if (!confPass) {
-        showToast("Passowrds Don't Match", "Please make sure password and confirm password match.");
+        showToast("Passwords Do Not Match", "Please make sure password and confirm password match.", STATUS_COLOR.RED, true, 5);
+        return;
     }
-    return;
-    createUserWithEmailAndPassword(auth, email, password)
+
+    if (!termsResult) {
+        showToast("Accept Terms", "You must read and accept our terms of service before continuing.", STATUS_COLOR.RED, true, 5);
+        return;
+    }
+
+    createUserWithEmailAndPassword(auth, createEmail.value.trim(), createPassword.value.trim())
         .then((userCredential) => {
             // Signed up 
             const user = userCredential.user;
-            // ...
+
+            //Upload user to database
+            set(ref_db(database, 'Users/' + user.uid), {
+                FirstName: createFName.value.trim(),
+                LastName: createLName.value.trim(),
+                Email: createEmail.value.trim(),
+            }).then(() => {
+                //Successfully uploaded, head to dashboard
+                window.location.href = "../dashboard/dashboard.html"
+            });
         })
         .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ..
+            showToast(error.code, error.message + ". Please try again", STATUS_COLOR.RED, true, 10);
         });
 }
