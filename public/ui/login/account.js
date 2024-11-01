65989a6fd26e93f6a284536c794b277bbd6bbe11
Brandon-Faulkner <bxf8125@mavs.uta.edu>
2024-11-01 09:50:40 -0500

    Added forgot password

--- public/ui/login/account.js
+++ public/ui/login/account.js
@@ -221,3 +221,21 @@ function createUserAccount() {
             showToast("Account Creation Error", "There was an error while trying to create your account. Please try again.", STATUS_COLOR.RED, true, 10);
         });
 }
+
+//Forgot password functions ------------------------------------------------
+const forgotPassEmail = document.getElementById("forgot-pass-email");
+const resetPasswordBtn = document.getElementById("reset-password");
+
+resetPasswordBtn?.addEventListener("click", () => {
+    if (forgotPassEmail.value.trim() == "") {
+        showToast("Missing Information", "You must enter a valid email address before you can reset your password.", STATUS_COLOR.RED, true, 8);
+        return;
+    }
+
+    sendPasswordResetEmail(auth, forgotPassEmail.value.trim()).then(() => {
+        showToast("Email Sent", "If your account exists, you should be receiving an email to reset your password.", STATUS_COLOR.GREEN, false);
+    }).catch((error) => {
+        console.log(error.code + ": " + error.message);
+        showToast("Error Sending Email", "There was an error sending an email to your address. Please try again.", STATUS_COLOR.RED, true, 8);
+    });
+});
