d063dbe46daaf9457d0ba1ba5058b22512893250
Brandon-Faulkner <faulknerb48@gmail.com>
2025-02-07 14:26:33 -0600

    Updated order tables

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,10 @@ module.exports = {
         'white-overlay': 'rgba(255,255,255,0.6)',
         'black': '#343434',
         'gray': '#D1D5DB',
-        'red': '#d53d3d'
+        'red': '#d53d3d',
+        'red-light': '#e27777',
+        'blue': '#1A6A99',
+        'blue-light': '#5f97b8'
       },
       fontFamily: {
         sans: ['Lilita One', 'sans-serif']
