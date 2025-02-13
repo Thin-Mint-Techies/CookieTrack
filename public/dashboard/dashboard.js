a04fe069a53e6fdb27cbcb22968fde16ff59311f
Brandon-Faulkner <bxf8125@mavs.uta.edu>
2025-02-12 18:20:53 -0600

    Did monthly suggestion done and working on more pages

--- public/dashboard/dashboard.js
+++ public/dashboard/dashboard.js
@@ -1,3 +1,22 @@
+//Get and update the current months predicted best seller
+const monthlyCookie = {
+    0: { name: "Adventurefuls", image: "/public/resources/images/adventurefuls.jpg" },
+    1: { name: "Thin Mints", image: "/public/resources/images/thin-mints.jpg" },
+    2: { name: "Peanut Butter Patties", image: "/public/resources/images/pb-patties.png" },
+    3: { name: "Trefoils", image: "/public/resources/images/trefoils.jpg" },
+};
+
+(function getCurrentMonthCookie() {
+    const currentMonth = new Date().getMonth();
+    let currentCookie =  monthlyCookie[currentMonth] || { name: "Unknown", image: "/public/resources/images/cookie_icon.png" };
+
+    let monthlyCookieName = document.getElementById("monthly-cookie-name");
+    let monthlyCookieImg = document.getElementById("monthly-cookie-img");
+
+    monthlyCookieName.textContent = currentCookie.name;
+    monthlyCookieImg.src = currentCookie.image;
+})();
+
 //Set the welcome greeting with users name
 document.addEventListener("authStateReady", (event) => {
     const user = event.detail;
